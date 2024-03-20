619da00f61fdfb763504762c2dab7cd4c724871b
sahosoft12 <132063023+sahosoft12@users.noreply.github.com>
2024-03-20 17:03:22 +0530

    Add files via upload

---
+++ userlist.js
@@ -0,0 +1,35 @@
+const userList = [];
+const userData = [];
+function connectUsers(data) {
+    const user = data;
+    userList.push(user);
+    console.log(userList);
+    return user;
+}
+
+function getUserByUserId(userid) {
+    
+    return userList.find(x => x.userId == userid);
+}
+function removeSocketId(id) {
+    // userList = userList.filter(x => x.socketId !== id);
+    const index = userList.findIndex(user => user.socketId === id);
+
+    if (index !== -1) {
+      userList.splice(index, 1)[0];
+    }
+}
+function setData(data){
+    userData.push(data);
+}
+function getData(userid){
+    return userData.find(x=> x.userId == userid);
+}
+
+module.exports = {
+    setData,
+    getData,
+    connectUsers,
+    getUserByUserId,
+    removeSocketId
+};
